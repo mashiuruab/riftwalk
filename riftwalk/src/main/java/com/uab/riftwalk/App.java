f30bce7ec371f5f6d1edabe57b871a4d13e75030
mashiur <mashiur@uab.com>
2017-10-28 05:50:25 -0500

    Commit done.

---
+++ riftwalk/src/main/java/com/uab/riftwalk/App.java
@@ -0,0 +1,13 @@
+package com.uab.riftwalk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
