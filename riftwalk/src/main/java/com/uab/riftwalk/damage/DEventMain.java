0c0a84e5d18d8ba40964766ca3ee68ce0fd945f7
Mashiur Rahman <mashiur@uab.edu>
2017-10-29 16:22:58 -0500

    Initial Implementation done for 2.1

--- riftwalk/src/main/java/com/uab/riftwalk/damage/DEventMain.java
+++ riftwalk/src/main/java/com/uab/riftwalk/damage/DEventMain.java
@@ -1,4 +1,37 @@
 package com.uab.riftwalk.damage;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 public class DEventMain {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        Job job = new Job(conf, "Damage done, received and heal Calculation");
+
+        job.setJarByClass(DEventMain.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(MapWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setMapperClass(DEventMapper.class);
+        job.setReducerClass(DEventReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
 }
--- riftwalk/src/main/java/com/uab/riftwalk/damage/DEventReducer.java
+++ riftwalk/src/main/java/com/uab/riftwalk/damage/DEventReducer.java
@@ -1,15 +1,40 @@
 package com.uab.riftwalk.damage;
 
+import com.google.gson.Gson;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DEventReducer extends Reducer<Text, MapWritable, Text, Text> {
+    private Gson gson = new Gson();
+
     @Override
-    protected void reduce(Text key, Iterable<MapWritable> values, Context context)
+    protected void reduce(Text characterId, Iterable<MapWritable> values, Context context)
             throws IOException, InterruptedException {
-        super.reduce(key, values, context);
+
+        double totalDoneDamage = 0;
+        double totalTakenDamage = 0;
+
+        for(MapWritable valueMap : values) {
+            DoubleWritable doneDamage = (DoubleWritable) valueMap.get(DEventMapper.DONE);
+            DoubleWritable takenDamage = (DoubleWritable) valueMap.get(DEventMapper.TAKEN);
+
+            totalDoneDamage += doneDamage.get();
+            totalTakenDamage += takenDamage.get();
+        }
+
+        double heal = Math.abs(totalDoneDamage - totalTakenDamage);
+
+        Map<String, Double> outMap = new HashMap<String, Double>();
+        outMap.put(DEventMapper.DONE.toString(), totalDoneDamage);
+        outMap.put(DEventMapper.TAKEN.toString(), totalTakenDamage);
+        outMap.put("heal", heal);
+
+        context.write(characterId, new Text(gson.toJson(outMap)));
     }
 }
